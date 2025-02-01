53a55768b2908a986ca6711f7198684078add3ab
Stian Zeljko Vrba <zvrba@zvrba.net>
2025-02-01 16:22:19 +0100

    WIP on unified tags.

---
+++ Pfm.Collections/Tree/JoinableTreeNode.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Podaga.PersistentCollections.Tree;
+
+/// <summary>
+/// Node of a binary search tree.  
+/// </summary>
+/// <typeparam name="TTag">Type of tags stored in the tree.  Use <see cref="TreeBalanceTag"/> when no custom tag is needed.</typeparam>
+/// <typeparam name="TValue">Type of values stored in the tree.</typeparam>
+[DebuggerDisplay("V={V} (S={Size}, R={Rank})")]
+public sealed class JoinableTreeNode<TTag, TValue> where TTag : struct, ITagTraits<TTag>
+{
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="transient">Transient value for this node.</param>
+    public JoinableTreeNode(ulong transient) {
+        X = transient;
+    }
+
+    /// <summary>
+    /// Transient tag.
+    /// </summary>
+    public readonly ulong X;
+
+    /// <summary>
+    /// Value contained in the node.
+    /// </summary>
+    public TValue V;
+
+    /// <summary>
+    /// Tag contained in the node.
+    /// </summary>
+    public TTag T;
+
+    /// <summary>
+    /// Left child, with key less than <see cref="V"/>.
+    /// </summary>
+    public JoinableTreeNode<TTag, TValue> L;
+
+    /// <summary>
+    /// Right child, with key larger than <see cref="V"/>.
+    /// </summary>
+    public JoinableTreeNode<TTag, TValue> R;
+
+    /// <summary>
+    /// Clones <c>this</c> if this' transient tag is different from <paramref name="transient"/>.
+    /// <returns>
+    /// New instance or <c>this</c>, depending on the transient tag.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining /*| MethodImplOptions.AggressiveOptimization*/)]
+    public JoinableTreeNode<TTag, TValue> Clone<TValueTraits>(ulong transient) where TValueTraits : IValueTraits<TValue>
+        => transient == X 
+        ? this 
+        : new(transient) { V = TValueTraits.Clone(V), T = T, L = L, R = R, };
+
+    /// <summary>
+    /// Updates <c>this</c> node's balance and monoidal tags.
+    /// WARNING: The update is in-place, so the node must have been cloned beforehand.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining /*| MethodImplOptions.AggressiveOptimization*/)]
+    public void Update<TTagTraits>() where TTagTraits : struct, ITagTraits<TTag>
+    {
+        var ltag = L?.T ?? TTag.Nil;
+        var rtag = R?.T ?? TTag.Nil;
+        TTagTraits.Combine(ltag, ref T, rtag);
+    }
+}
+
---
+++ Pfm.Collections/Tree/Traits.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Podaga.PersistentCollections.Tree;
+
+/// <summary>
+/// Allows for implementation of custom monoidal tags attached to nodes.
+/// </summary>
+/// <typeparam name="TTag">
+/// Tag type; must be a mutable struct.  Struct requirement simplifies cloning of nodes.  Mutability is required because the tag
+/// must incorporate mutable properties.
+/// </typeparam>
+public interface ITagTraits<TTag> where TTag : struct
+{
+    /// <summary>
+    /// Tag value corresponding to <c>null</c> node.
+    /// </summary>
+    abstract static TTag Nil { get; }
+
+    /// <summary>
+    /// The node's rank.  MUST NOT be touched by user code.
+    /// </summary>
+    int Rank { get; set; }
+
+    /// <summary>
+    /// Size is the count of nodes under and including this node.  MUST NOT be touched by user code.
+    /// </summary>
+    int Size { get; set; }
+
+    /// <summary>
+    /// Computes <c>result = left + result + right</c>.  The computation of <see cref="Rank"/> and <see cref="Size"/>
+    /// must be delegated to the tree's concrete balance tag.
+    /// </summary>
+    /// <param name="left">Tag value corresponding to the left branch.  <see cref="Nil"/> if there is no left branch.</param>
+    /// <param name="result">Tag value corresponding to the current node.</param>
+    /// <param name="right">Tag value corresponding to the right branch.  <see cref="Nil"/> if there is no right branch.</param>
+    abstract static void Combine(TTag left, ref TTag result, TTag right);
+}
+
+/// <summary>
+/// Value traits determine how values are interpreted by the tree.  These are independent of tree mechanics and
+/// may be reused with different tree implementations.
+/// </summary>
+/// <typeparam name="TValue">Value type of the tree.</typeparam>
+public interface IValueTraits<TValue>
+{
+    /// <summary>
+    /// Compares two values.  Must implement a total order.
+    /// </summary>
+    /// <param name="left">Left value to compare.</param>
+    /// <param name="right">Right value to compare.</param>
+    /// <returns>
+    /// Negative, zero or positive integer when <paramref name="left"/> is, respectively smaller than, equal
+    /// to, or greater than <paramref name="right"/>.
+    /// </returns>
+    abstract static int Compare(TValue left, TValue right);
+
+    /// <summary>
+    /// Clones the value whenever a node is cloned due to modification.
+    /// Default implementation is a no-op, i.e., just returns <paramref name="value"/>.
+    /// </summary>
+    /// <returns>The cloned value.</returns>
+    virtual static TValue Clone(TValue value) => value;
+}
+
+/// <summary>
+/// Every joinable tree must implement this interface.  The join algorithm itself is independent of value traits.
+/// </summary>
+/// <typeparam name="TTag">Tag type.</typeparam>
+/// <typeparam name="TValue">Type of values stored in the tree.</typeparam>
+public interface ITreeTraits<TTag, TValue>
+    where TTag : struct, ITagTraits<TTag>
+{
+    /// <summary>
+    /// Value provided to <see cref="CombineRanks(int, int)(int, int)"/> when node is missing a left or right child.
+    /// Default implementation returns 0, which should almost always be OK because, according to the paper, ranks
+    /// are monotonic wrt tree height.
+    /// </summary>
+    virtual static int NilRank => 0;
+
+    /// <summary>
+    /// Combines ranks from child nodes.  When either child is missing, the method will be invoked with <see cref="NilRank"/>.
+    /// </summary>
+    /// <param name="left">Rank of the left child.</param>
+    /// <param name="right">Rank of the right child.</param>
+    abstract static int CombineRanks(int left, int right);
+
+    /// <summary>
+    /// 3-way join is the core tree algorithm on which all other operations are based.
+    /// </summary>
+    /// <param name="transient">Transient tag used for lazy cloning during tree modifications.</param>
+    /// <param name="left">Left tree to join.</param>
+    /// <param name="middle">The join pivot to insert in the result.</param>
+    /// <param name="right">Right tree to join.</param>
+    /// <returns>
+    /// Tree that has same entries and inorder traversal as the node <c>(left, middle, right)</c>.
+    /// </returns>
+    /// <remarks>
+    /// When the tree is not persistent, this operation is destructive to all inputs.
+    /// </remarks>
+    abstract static JoinableTreeNode<TTag, TValue> Join(
+        ulong transient,
+        JoinableTreeNode<TTag, TValue> left,
+        JoinableTreeNode<TTag, TValue> middle,
+        JoinableTreeNode<TTag, TValue> right);
+
+    /// <summary>
+    /// This method must validate the tree's structure invariant.
+    /// Mainly for use in stress-tests.
+    /// </summary>
+    /// <exception cref="NotImplementedException">Thrown when a violation of the structure invariant is detected.</exception>
+    abstract static void ValidateStructure(JoinableTreeNode<TTag, TValue> root);
+}
