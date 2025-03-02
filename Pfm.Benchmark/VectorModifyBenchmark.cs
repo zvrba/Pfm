4d55b2f9e0b2935c9a810fd1144233232d742c99
Stian Z. Vrba <stian.vrba@outlook.com>
2025-03-02 16:25:47 +0100

    Add Grow benchmark for list and vector.

--- Pfm.Benchmark/VectorModifyBenchmark.cs
+++ Pfm.Benchmark/VectorModifyBenchmark.cs
@@ -48,4 +48,18 @@ public class VectorModifyBenchmark
         for (int i = 0; i < trie.Count; ++i)
             trie[i] += 1;
     }
+
+    [Benchmark]
+    public void GrowList() {
+        var x = new List<int>();
+        for (int i = 0; i < Size; ++i)
+            x.Add(i);
+    }
+
+    [Benchmark]
+    public void GrowVector() {
+        var x = new Vector<int>(new(5, 5));
+        for (int i = 0; i < Size; ++i)
+            x.Push(i);
+    }
 }
