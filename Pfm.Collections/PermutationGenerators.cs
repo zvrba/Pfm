9f3b4c59fb49fabad7c736b17bc74da82df67e25
Stian Z. Vrba <stian.vrba@outlook.com>
2025-03-23 11:20:24 +0100

    Documentation.

--- Pfm.Collections/PermutationGenerators.cs
+++ Pfm.Collections/PermutationGenerators.cs
@@ -9,10 +9,17 @@ public static class PermutationGenerators
 {
     private static readonly Random random = new Random(3141);
 
+    /// <summary>
+    /// This array contains delegates for all predefined patterns.
+    /// </summary>
     public static readonly Action<int[]>[] Generators = new Action<int[]>[] {
         Ascending, Descending, Balanced, ZigZag, Shifted, Bitonic, Random
     };
 
+    /// <summary>
+    /// Fills <paramref name="a"/> with a random permutation of elements from 0 up to <c>a.Length - 1</c>.
+    /// </summary>
+    /// <param name="a">Array to fill.</param>
     public static void Random(int[] a) {
         for (int i = 0; i < a.Length; ++i)
             a[i] = i;
@@ -22,16 +29,28 @@ public static class PermutationGenerators
         }
     }
 
+    /// <summary>
+    /// Fills <paramref name="a"/> with elements in order 0, 1, 2, ..., <c>a.Length-1</c>.
+    /// </summary>
+    /// <param name="a">Array to fill.</param>
     public static void Ascending(int[] a) {
         for (var i = 0; i < a.Length; ++i)
             a[i] = i;
     }
 
+    /// <summary>
+    /// Fills <paramref name="a"/> with elements in order <c>a.Length-1</c>, ..., 2, 1, 0.
+    /// </summary>
+    /// <param name="a">Array to fill.</param>
     public static void Descending(int[] a) {
         for (var i = 0; i < a.Length; ++i)
             a[i] = a.Length - i - 1;
     }
 
+    /// <summary>
+    /// Fills <paramref name="a"/> with a "balanced" permutation of elements from 0 to <c>a.Length-1</c>.
+    /// </summary>
+    /// <param name="a">Array to fill.</param>
     public static void Balanced(int[] a) {
         int i = 0;
         Recurse(0, a.Length - 1);
@@ -47,6 +66,10 @@ public static class PermutationGenerators
         }
     }
 
+    /// <summary>
+    /// Fills <paramref name="a"/> with a "zig-zag" permutation of elements from 0 to <c>a.Length-1</c>.
+    /// </summary>
+    /// <param name="a">Array to fill.</param>
     public static void ZigZag(int[] a) {
         int zig = 0, zag = a.Length - 1;
         for (int i = 0; i < a.Length; ++i) {
@@ -55,6 +78,10 @@ public static class PermutationGenerators
         }
     }
 
+    /// <summary>
+    /// Fills <paramref name="a"/> with a "shifted" (by half length) permutation of elements from 0 to <c>a.Length-1</c>.
+    /// </summary>
+    /// <param name="a">Array to fill.</param>
     public static void Shifted(int[] a) {
         var half = a.Length / 2;
         int i, k;
@@ -64,6 +91,10 @@ public static class PermutationGenerators
             a[i] = k++;
     }
 
+    /// <summary>
+    /// Fills <paramref name="a"/> with a "bitonic" permutation of elements from 0 to <c>a.Length-1</c>.
+    /// </summary>
+    /// <param name="a">Array to fill.</param>
     public static void Bitonic(int[] a) {
         int i, k;
         for (i = 0, k = 0; k < a.Length; ++i, k += 2)
